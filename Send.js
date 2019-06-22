6ccbf2586cfae4d1c3833ed0228952a45fccbe01
MarcosArthur <arturcruz0@gmail.com>
2019-06-22 13:03:25 -0300

    melhorias

--- Send.js
+++ Send.js
@@ -1,38 +1,36 @@
 class Send {
 
-  constructor(form, route, elementAlert) {
-    this.form = document.querySelector(form);
-    this.elementAlert = document.querySelector(elementAlert);
-    this.route = route;
-    this.response;
-    this.eventSubmitForm();
+  static toSend(form, route, method, operation) {
+    document.querySelector(form).addEventListener('submit', e => {
+      e.preventDefault();
+      fetch(route, {
+          method: method,
+          body: new FormData(document.querySelector(form))
+        })
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+
+          new Error('Error to send');
+          return;
+        })
+        .then(response => {
+          operation(response);
+        })
+        .catch(e => {
+          new Error(e.statusText);
+        })
+    })
+
   }
 
-  eventSubmitForm() {
-    this.form.addEventListener('submit', this.send.bind(this));
-  }
-
-  send(event) {
-    event.preventDefault();
-
-    fetch(this.route, {
-      method: "POST",
-      body: new FormData(document.querySelector(this.form))
-    })
-    .then(response => {
-
-      if (response.ok) {
-        return response.json()
-      }
-
-      new Error(response.statusText);
-      return;
-
-    })
-    .then(response => {
-      this.response = response;
-    })
-
+  static get(route, operation = null) {
+    fetch(route)
+      .then(response => response.json())
+      .then(response => {
+        operation(response);
+      })
   }
 
 }
